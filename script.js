da35884715bbbb583bfaa4758502388aa26ac328
tamayo2 <juan.tamayo3@utp.edu.co>
2024-04-03 02:39:20 -0500

    agregue codigo en java para funcionalidad de la pagina

---
+++ script.js
@@ -0,0 +1,46 @@
+var swiper = new Swiper(".mySwiper-1", {
+    slidesPerView:1,
+    spaceBetween: 30,
+    loop:true,
+    pagination: {
+        el:".swiper-pagination",
+        clickable: true,
+    },
+    navigation: {
+        nextEl:".swiper-button-next",
+        prevEl:".swiper-button-prev",
+    }
+});
+
+var swiper = new Swiper(".mySwiper-2", {
+    slidesPerView:3,
+    spaceBetween: 20,
+    loop:true,
+    loopFillGroupWithBlank:true,
+    navigation: {
+        nextE1:".swiper-button-next",
+        prevE1:".swiper-button-prev",
+    },
+    breakpoints : {
+        0: {
+            slidesPerView:1,
+        },
+        520: {
+            slidesPerView:2,
+        },
+        950: {
+            slidesPerView:3,
+        }
+    }
+});
+
+let tabInputs = document.querySelectorAll(".tabInput");
+
+tabInputs.forEach(function(input){
+    input.addEventListener('change', function(){
+        let id = input.ariaValueMax;
+        let thisSwiper = document.getElementById('swiper' + id);
+        thisSwiper.swiper.update();
+    })
+
+});
